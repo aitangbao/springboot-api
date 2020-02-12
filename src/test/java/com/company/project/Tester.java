c7d120073546eec3b2cd95cf4d18e2d397866400
wenbin <1104302665@qq.com>
2020-02-12 15:34:44 +0800

    init

--- src/test/java/com/company/project/Tester.java
+++ src/test/java/com/company/project/Tester.java
@@ -1,19 +1,22 @@
 package com.company.project;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
- * 单元测试继承该类即可
+ * 单元测试
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-@Transactional
-@Rollback
-public abstract class Tester {}
+public class Tester {
+
+    @Test
+    public void test() {
+
+    }
+}
 
 
 
